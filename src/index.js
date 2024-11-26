efc9a791f58f7ea45df94c57bf2930d7bf626827
SaegusaMayumi1234 <theresanojulianl1234@gmail.com>
2024-11-26 22:35:28 +0700

    fix: error caused by wrong variable name

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const game8 = require('./scrapper/game8');
 const genshinImpactFandom = require('./scrapper/genshinImpactFandom');
 
 game8.execute().then(data => console.log(data));
-fandom.execute().then(data => console.log(data));
+genshinImpactFandom.execute().then(data => console.log(data));
