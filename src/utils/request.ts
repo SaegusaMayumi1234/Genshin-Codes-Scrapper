88f22f44ae375a7a8d04ed180c1cfba0c7208704
Theresano Julian Laksono <theresano.laksono@binus.edu>
2025-01-07 17:54:58 +0700

    fix: Try adding user agent

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import ApiError from './apiError';
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+  
+});
 
 export default {
   async getSiteData(url: string): Promise<any> {
@@ -9,6 +11,9 @@ export default {
       const response = await axiosInstance({
         url,
         method: 'GET',
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.3'
+        }
       });
 
       return response.data;
